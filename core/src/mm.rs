38535f629454ac3268b777f761a623be7e71224f
eternalcomet <mydragonos@outlook.com>
2025-04-10 20:33:36 +0800

    add iozone

--- core/src/mm.rs
+++ core/src/mm.rs
@@ -119,6 +119,7 @@ pub fn load_user_app(
         )?;
 
         if interp_path == "/lib/ld-linux-riscv64-lp64.so.1"
+            || interp_path == "/lib/ld-musl-riscv64.so.1"
             || interp_path == "/lib64/ld-linux-loongarch-lp64d.so.1"
             || interp_path == "/lib64/ld-linux-x86-64.so.2"
             || interp_path == "/lib/ld-linux-aarch64.so.1"
