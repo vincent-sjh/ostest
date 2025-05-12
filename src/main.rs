a4fa07cc6abde29b6e91620dacd02ff98f146339
eternalcomet <mydragonos@outlook.com>
2025-05-12 22:38:12 +0800

    deal with pwd

--- src/main.rs
+++ src/main.rs
@@ -46,7 +46,7 @@ fn main() {
 
         let envs = vec![
             "PATH=/bin".to_string(),
-            "LD_LIBRARY_PATH=/lib/".to_string(),
+            "LD_LIBRARY_PATH=/lib:/lib64".to_string(),
             // "LD_DEBUG=all".to_string(),
         ];
 
